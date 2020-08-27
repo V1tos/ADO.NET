cc4d46edf5063918ae574251ea5055595cfdc11f
V1tos <victor.palamarchuk@gmail.com>
2020-08-28 00:30:05 +0300

    Import Image

---
+++ WF1 - DataAdapter/WF - Task/WF - Task/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WF___Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoadImg = new System.Windows.Forms.Button();
+            this.btnFill = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 285);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnLoadImg
+            // 
+            this.btnLoadImg.Location = new System.Drawing.Point(12, 391);
+            this.btnLoadImg.Name = "btnLoadImg";
+            this.btnLoadImg.Size = new System.Drawing.Size(113, 47);
+            this.btnLoadImg.TabIndex = 1;
+            this.btnLoadImg.Text = "Load image";
+            this.btnLoadImg.UseVisualStyleBackColor = true;
+            this.btnLoadImg.Click += new System.EventHandler(this.btnLoadImg_Click);
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(160, 391);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(99, 47);
+            this.btnFill.TabIndex = 2;
+            this.btnFill.Text = "Fill";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(298, 391);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(99, 47);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 301);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 84);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(448, 391);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(448, 358);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnFill);
+            this.Controls.Add(this.btnLoadImg);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoadImg;
+        private System.Windows.Forms.Button btnFill;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WF1 - DataAdapter/WF - Task/WF - Task/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF___Task
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+        SqlConnection connection = null;
+        SqlDataAdapter adapter = null;
+        SqlCommandBuilder builder = null;
+        DataSet set = null;
+        byte[] convImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadImg_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.bmp; *.jpg; *.jpeg, *.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = dialog.FileName;
+                pictureBox1.Image = Image.FromFile(fileName);
+                ConvertImageToByteArr(pictureBox1.Image);
+            }
+
+        }
+
+        private void ConvertImageToByteArr(Image image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, image.RawFormat);
+                convImage =  ms.ToArray();
+            }
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter("select * from Books", connection);
+            builder = new SqlCommandBuilder(adapter);
+
+            set = new DataSet();
+            adapter.Fill(set);
+            dataGridView1.DataSource = set.Tables[0];
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            set.Tables[0].Rows[int.Parse(textBox1.Text)]["Picture"] = convImage;
+
+            SqlCommand command = new SqlCommand($"Update Books Set Picture = @p1 where Id = @p2");
+
+            command.Parameters.AddWithValue("@p1", convImage);
+            command.Parameters.AddWithValue("@p2", int.Parse(textBox1.Text));
+
+            adapter.UpdateCommand = command;
+            adapter.Update(set);
+        }
+    }
+}
