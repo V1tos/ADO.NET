85c18dc1937453a3e6048be55832d383e9f5ce0e
V1tos <victor.palamarchuk@gmail.com>
2020-09-12 01:22:21 +0300

    AsyncDelegates added

---
+++ WPF 7 - CRUD (AsyncDelegates)/WPF 7 - CRUD (AsyncDelegates)/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_7___CRUD__AsyncDelegates_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString);
+        SqlConnection connection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            builder.AsynchronousProcessing = true;
+            connection = new SqlConnection(builder.ConnectionString);
+            OpenConection(connection);
+            lbStudents.ItemsSource = Students;
+        }
+
+        private void OpenConection(SqlConnection connection)
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1(null, connection);
+            window.ShowDialog();
+            ReadFromStudent();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbStudents.SelectedItem == null)
+                return;
+            Student updateStudent = lbStudents.SelectedItem as Student;
+            Window1 window = new Window1(updateStudent, connection);
+            window.ShowDialog();
+            ReadFromStudent();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbStudents.SelectedItem == null)
+                return;
+
+            Student deleteStudent = lbStudents.SelectedItem as Student;
+            DeleteFromStudent(deleteStudent);
+            ReadFromStudent();
+        }
+        private void DeleteFromStudent(Student student)
+        {
+            int deleteStudentId = student.Id;
+            string cmdDelete = $"Delete Student Where Id = {deleteStudentId}";
+            SqlCommand command = new SqlCommand(cmdDelete, connection);
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            ReadFromStudent();
+        }
+
+       
+        private void ReadFromStudent()
+        {
+            string cmdRead = "Select * From Student";
+            SqlCommand command = new SqlCommand(cmdRead, connection);
+            Students.Clear();
+            var reader = command.BeginExecuteReader(ReaderCallback, command);
+
+        }
+
+        private void ReaderCallback(IAsyncResult ar)
+        {
+            var result = (SqlCommand)ar.AsyncState;
+            var reader = result.EndExecuteReader(ar);
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        Students.Add(new Student
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Surname = reader.GetString(2),
+                            GroupId = reader.GetValue(3) as Nullable<int>
+                        });
+                    });
+                }
+            }
+
+            reader.Close();
+        }
+    }
+}
+
---
+++ WPF 7 - CRUD (AsyncDelegates)/WPF 7 - CRUD (AsyncDelegates)/Window1.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_7___CRUD__AsyncDelegates_
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        List<Group> groups = new List<Group>();
+        public Student Student { get; set; }
+        bool isCreate = true;
+        SqlConnection connection;
+
+        public Window1(Student student, SqlConnection connection)
+        {
+            InitializeComponent();
+            this.connection = connection;
+
+
+            ReadFromGroups();
+            cbGroup.ItemsSource = groups;
+            Student = new Student();
+            if (student != null)
+            {
+                Student = student;
+                isCreate = false;
+                FillFields(Student);
+            }
+
+        }
+        private void ReadFromGroups()
+        {
+            groups.Clear();
+            string cmdRead = "Select * from Groups";
+            SqlCommand command = new SqlCommand(cmdRead, connection);
+            command.BeginExecuteReader(ReaderCallback, command);
+        }
+
+        private void ReaderCallback(IAsyncResult ar)
+        {
+            var result = (SqlCommand)ar.AsyncState;
+            var reader = result.EndExecuteReader(ar);
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        groups.Add(new Group
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1)
+                        });
+                    });
+                }
+            }
+
+            reader.Close();
+        }
+
+        private void FillFields(Student student)
+        {
+            tbName.Text = student.Name;
+            tbSurname.Text = student.Surname;
+            cbGroup.SelectedItem = groups.Find(x => x.Id == student.GroupId);
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Student.Name = tbName.Text;
+            Student.Surname = tbSurname.Text;
+            Student.GroupId = groups.Find(x => x.Name == cbGroup.Text).Id;
+            if (isCreate)
+                AddStudent(Student);
+            else
+                UpdateStudent(Student);
+
+            this.Close();
+        }
+
+        private void AddStudent(Student student)
+        {
+            string cmdAdd = $"insert into Student values(@name, @surname, @idGroup)";
+            SqlCommand command = new SqlCommand(cmdAdd, connection);
+            command.Parameters.AddWithValue("@name", student.Name);
+            command.Parameters.AddWithValue("@surname", student.Surname);
+            command.Parameters.AddWithValue("@idGroup", student.GroupId);
+            command.ExecuteNonQuery();
+        }
+
+        private void UpdateStudent(Student student)
+        {
+            string cmdUpdate = $"update student set Name = @name, Surname = @surname, IdGroup = @idGroup where id = @id";
+            SqlCommand command = new SqlCommand(cmdUpdate, connection);
+            command.Parameters.AddWithValue("@id", student.Id);
+            command.Parameters.AddWithValue("@name", student.Name);
+            command.Parameters.AddWithValue("@surname", student.Surname);
+            command.Parameters.AddWithValue("@idGroup", student.GroupId);
+            command.ExecuteNonQuery();
+        }
+
+    }
+}
